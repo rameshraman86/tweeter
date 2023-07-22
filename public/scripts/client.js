59bba929c0e3af636f7fb3246128efa77e9eef10
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-21 20:34:53 -0400

    character count dynamically rests to 140 after submitting tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,142 +3,119 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+{
 
-
-$(document).ready(function() {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-  const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      const $formattedTweetData = createTweetElement(tweet);
-      $('#tweets-container').append($formattedTweetData);
-    }
-  };
-
-
-  const createTweetElement = function(tweet) {
-    const { content, created_at, user } = tweet;
-    const timePassed = timeago.format(created_at);
-    const avatar = user.avatars;
-    const handle = user.handle;
-    const name = user.name;
-
-    const dataAttr = 0;
-
-    const $article = $('<article>').addClass('tweet');
-
-    const $tweetHeader = $('<div>').addClass('tweet-header').attr('id', "tweet-header");
-    const $leftSideOfHeader = $('<div>').addClass('tweet-header-left');
-
-    const $avatar = $('<img>').attr('src', sanitize(avatar));
-    const $fullName = $('<span>').text(sanitize(name));
-    $leftSideOfHeader.append($avatar, $fullName);
-    const $handle = $('<span>').attr('id', "user-handle").text(sanitize(handle));
-    $tweetHeader.append($leftSideOfHeader, $handle);
-
-
-    const $content = $('<p>').text(sanitize(content.text));
-    const $hr = $('<hr>');
-
-    const $footer = $('<footer>');
-    const $duration = $('<span>').text(sanitize(timePassed));
-
-
-    const $footerRightSideDiv = $('<div>');
-
-    const $flag = $('<i>').addClass('fa-regular fa-flag');
-    const $retweet = $('<i>').addClass('fa-regular fa-circle');
-    const $heart = $('<i>').addClass('fa-regular fa-heart');
-
-    $footerRightSideDiv.append($flag, $retweet, $heart);
-    $footer.append($duration, $footerRightSideDiv);
-
-    $article.append($tweetHeader, $content, $hr, $footer);
-
-    return $article;
-  };
-
-  function sanitize(text) {
-    const element = document.createTextNode(text);
-    const div = document.createElement('div');
-    div.appendChild(element);
-    return div.innerHTML;
-  }
-
-
-  const errorMessageElement = function(errorType) {
-    const errorMessages = {
-      zero: "Don't be shy. Use your words.",
-      exceededLength: "Uh oh!!! Tweet length exceeds 140 characters. Buy \"tweeter pro\" to enter more."
+  $(document).ready(function() {
+    const renderTweets = function(tweets) {
+      for (const tweet of tweets) {
+        const $formattedTweetData = createTweetElement(tweet);
+        $('#tweets-container').append($formattedTweetData);
+      }
     };
-    return $('<h3>').attr('id', 'error-message').text(errorMessages[errorType]);
-  };
 
 
-  const loadTweets = function() {
-    $.get("/tweets")
-      .then(renderTweets)
-      .catch(error => console.log('Error details: ' + error));
-  };
-  loadTweets();
+    const createTweetElement = function(tweet) {
+      const { content, created_at, user } = tweet;
+      const timePassed = timeago.format(created_at);
+      const avatar = user.avatars;
+      const handle = user.handle;
+      const name = user.name;
+
+      const $article = $('<article>').addClass('tweet');
+
+      const $tweetHeader = $('<div>').addClass('tweet-header').attr('id', "tweet-header");
+      const $leftSideOfHeader = $('<div>').addClass('tweet-header-left');
+
+      const $avatar = $('<img>').attr('src', sanitize(avatar));
+      const $fullName = $('<span>').text(sanitize(name));
+      $leftSideOfHeader.append($avatar, $fullName);
+      const $handle = $('<span>').attr('id', "user-handle").text(sanitize(handle));
+      $tweetHeader.append($leftSideOfHeader, $handle);
 
 
-  const $form = $('form');
-  $form.on('submit', function(event) {
-    const $tweetDataSerialized = $form.serialize();
-    const $tweet = $("#tweet-text")[0].value;
-    event.preventDefault();
+      const $content = $('<p>').text(sanitize(content.text));
+      const $hr = $('<hr>');
 
-    if ($("#error-message")) {
-      $("#error-message").hide().remove();
+      const $footer = $('<footer>');
+      const $duration = $('<span>').text(sanitize(timePassed));
+
+
+      const $footerRightSideDiv = $('<div>');
+
+      const $flag = $('<i>').addClass('fa-regular fa-flag');
+      const $retweet = $('<i>').addClass('fa-regular fa-circle');
+      const $heart = $('<i>').addClass('fa-regular fa-heart');
+
+      $footerRightSideDiv.append($flag, $retweet, $heart);
+      $footer.append($duration, $footerRightSideDiv);
+
+      $article.append($tweetHeader, $content, $hr, $footer);
+
+      return $article;
+    };
+
+    function sanitize(text) {
+      const element = document.createTextNode(text);
+      const div = document.createElement('div');
+      div.appendChild(element);
+      return div.innerHTML;
     }
 
-    if ($tweet.length === 0) {
-      $("form").before(errorMessageElement("zero"));
-      $("#error-message").slideToggle();
-      return;
-    }
-    if ($tweet.length > 140) {
-      $("form").before(errorMessageElement("exceededLength"));
-      $("#error-message").slideToggle();
-      return;
-    }
 
-    $.post('/tweets', $tweetDataSerialized)
-      .then(() => {
-        $("#tweet-text").val('');
+    const errorMessageElement = function(errorType) {
+      const errorMessages = {
+        zero: "Don't be shy. Use your words.",
+        exceededLength: "Uh oh!!! Tweet length exceeds 140 characters. Buy \"tweeter pro\" to enter more."
+      };
+      return $('<h3>').attr('id', 'error-message').text(errorMessages[errorType]);
+    };
 
-        $.get('/tweets')
-          .then((res) => {
-            $("#tweets-container").prepend(createTweetElement(res[0]))
-          })
-          .catch((err) => console.log('Error: ' + err));
-      })
-      .catch((err) => console.log('Error: ' + err));
+
+    const loadTweets = function() {
+      $.get("/tweets")
+        .then(renderTweets)
+        .catch(error => console.log('Error details: ' + error));
+    };
+    loadTweets();
+
+
+    const $form = $('form');
+    $form.on('submit', function(event) {
+
+      const $tweetDataSerialized = $form.serialize();
+      const $tweet = $("#tweet-text")[0].value;
+      event.preventDefault();
+
+      if ($("#error-message")) {
+        $("#error-message").hide().remove();
+      }
+
+      if ($tweet.length === 0) {
+        $("form").before(errorMessageElement("zero"));
+        $("#error-message").slideToggle();
+        return;
+      }
+      if ($tweet.length > 140) {
+        $("form").before(errorMessageElement("exceededLength"));
+        $("#error-message").slideToggle();
+        return;
+      }
+
+      $.post('/tweets', $tweetDataSerialized)
+        .then(() => {
+          $("#tweet-text").val('');
+          $("#counter").val('140');
+
+          $.get('/tweets')
+            .then((res) => {
+              $("#tweets-container").prepend(createTweetElement(res[res.length - 1]));
+            })
+            .catch((err) => console.log('Error: ' + err));
+        })
+        .catch((err) => console.log('Error: ' + err));
+    });
+
+
   });
-});
+
+}
