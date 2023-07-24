74197fa49a03c5fc9a5d9402a3dc6574166d586e
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-24 15:20:49 -0400

    removed empty spaces

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $("#tweet-text").on('input', function(e) {
-    var $tweet = $(this);
-    var $counter = $("#counter");
+    let $tweet = $(this);
+    let $counter = $("#counter");
 
     const allowedTweetLength = 140;
-    var remainingChars = allowedTweetLength - $tweet.val().length;
-
+    let remainingChars = allowedTweetLength - $tweet.val().length;
+    
     if (remainingChars < 0) {
       $counter.removeClass("counter").addClass("counterRed");
     } else {
